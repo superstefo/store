890e2d637627f01828b4a9dcd77e187ba533e383
Stefan Stefanov <superstefo@google.com>
2021-08-09 13:16:17 +0300

    Updates

---
+++ precache-manifest.7fcdfd231e4ac6fa5c41da7f03c2bfe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b1ef0ef02457c1663d9acdab5b71c2",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "a1a03d7d1b39d7584db1",
+    "url": "/store/static/css/2.45a4569d.chunk.css"
+  },
+  {
+    "revision": "a1a03d7d1b39d7584db1",
+    "url": "/store/static/js/2.e1d8fd11.chunk.js"
+  },
+  {
+    "revision": "dff38bf37fc9eda4acb236ebba3264fd",
+    "url": "/store/static/js/2.e1d8fd11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3624dd0f6679a896aaa0",
+    "url": "/store/static/js/main.3d7aa116.chunk.js"
+  },
+  {
+    "revision": "9022ecec7728c3c2ef78",
+    "url": "/store/static/js/runtime-main.3fb5337e.js"
+  }
+]);
